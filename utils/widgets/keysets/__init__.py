aecd414c3c9f38aeafe763667f285cc6e65edfbb
flossbud <flossbud27@gmail.com>
2026-07-07 21:00:01 -0400

    feat(keysets): scaffold keysets widget package

---
+++ utils/widgets/keysets/__init__.py
@@ -0,0 +1,5 @@
+"""Keysets surface (Settings category): game picker + Split editor.
+
+Recreates Redesign/'TTMT - Keyset Redesign + Reimplementation' on the v2 kit.
+Public entry point: KeysetsPage.
+"""
